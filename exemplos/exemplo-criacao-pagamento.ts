cf4b3aa0cf43161aba498745feb2fd3c82b6a97f
Carlos Jaime <jaime_andrek@hotmail.com>
2025-11-05 10:40:15 -0300

    exemplos mercado pago

---
+++ exemplos/exemplo-criacao-pagamento.ts
@@ -0,0 +1,26 @@
+import { MercadoPagoConfig, Preference } from 'mercadopago';
+
+const client = new MercadoPagoConfig({ accessToken: 'YOUR_ACCESS_TOKEN' });
+const preference = new Preference(client);
+const result = await preference.create({
+    body: {
+        items: [
+            {
+                id: '1234',
+                title: 'Meu produto',
+                quantity: 1,
+                unit_price: 2000
+            }
+        ],
+        external_reference: "codigo unico seu",
+        back_urls: {
+            success: 'http://localhost:3000/success',
+            failure: 'http://localhost:3000/failure',
+            pending: 'http://localhost:3000/pending'
+        },
+        auto_return: 'approved'
+    }
+})
+
+//url gerada
+result.init_point
